56483acdc0f24c7e76da574b8c4279fb6c13422c
tomeksproch <tomek.sproch@gmail.com>
2023-03-05 12:54:30 +0100

    fix api 2

--- src/context/ContextCampaignsData.js
+++ src/context/ContextCampaignsData.js
@@ -8,7 +8,7 @@ export function ContextCampaignsProvider({ children }) {
   const [budget, setBudget] = useState();
   const [cities, setCities] = useState();
 
-  const API_URL = "https://product-campaign-app-api.herokuapp.com";
+  // const API_URL = "https://product-campaign-app-api.herokuapp.com";
 
   useEffect(() => {
     getCampaigns();
@@ -16,26 +16,38 @@ export function ContextCampaignsProvider({ children }) {
   }, []);
 
   const getCampaigns = async () => {
-    const { data: res } = await axios.get(`${API_URL}/campaigns`);
+    const { data: res } = await axios.get(
+      `https://product-campaign-app-api.herokuapp.com/campaigns`
+    );
     setCampaigns(res);
 
-    const { data: budgetRes } = await axios.get(`${API_URL}/budget`);
+    const { data: budgetRes } = await axios.get(
+      `https://product-campaign-app-api.herokuapp.com/budget`
+    );
     setBudget(budgetRes.budget);
   };
 
   const getCities = async () => {
-    const { data: citiesRes } = await axios.get(`${API_URL}/cities`);
+    const { data: citiesRes } = await axios.get(
+      `https://product-campaign-app-api.herokuapp.com/cities`
+    );
     setCities(citiesRes);
   };
 
   const handleDeleteCampaigns = async (id) => {
-    await axios.delete(`${API_URL}/campaigns`, { data: { id } });
+    await axios.delete(
+      `https://product-campaign-app-api.herokuapp.com/campaigns`,
+      { data: { id } }
+    );
     getCampaigns();
   };
 
   const handleAddCampaign = async (newCampaign) => {
     const response = await axios
-      .post(`${API_URL}/campaigns`, newCampaign)
+      .post(
+        `https://product-campaign-app-api.herokuapp.com/campaigns`,
+        newCampaign
+      )
       .catch((error) => {
         return error;
       });
